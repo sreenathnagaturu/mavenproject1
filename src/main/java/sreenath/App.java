fa80b9f9839f799fe4a46b2e5b7fb4ff851cb36e
sreenath <nagaturusreenath@gmail.com>
2023-09-25 14:52:17 +0530

    sample project

---
+++ src/main/java/sreenath/App.java
@@ -0,0 +1,13 @@
+package sreenath;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
